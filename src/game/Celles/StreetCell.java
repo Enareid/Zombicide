b83e1808f3da9bbdc0bf49713ab4c1cd062b6da8
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-01-25 13:39:13 +0100

    constructor StreetCell() in class StreetCell.java

--- src/game/Celles/StreetCell.java
+++ src/game/Celles/StreetCell.java
@@ -3,5 +3,14 @@ package src.game.Celles;
 import src.game.Cell;
 
 public class StreetCell extends Cell{
+    protected boolean canSpawn;
+
+    /**
+     * Builds a new street cell. 
+     */
+    public StreetCell() {
+        super();
+        this.canSpawn = true;
+    }
     
 }
