534ee77c5cfc1080198c706ea4af1d08f38fe7ea
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-01-25 13:50:53 +0100

    constructor BuildingCell() in class BuildingCell.java

--- src/game/Celles/BuildingCell.java
+++ src/game/Celles/BuildingCell.java
@@ -3,5 +3,13 @@ package src.game.Celles;
 import src.game.Cell;
 
 public class BuildingCell extends Cell{
-    
+    protected List<Door> doors;
+
+    /**
+     * Builds a new building cell. 
+     */
+    public BuildingCell() {
+        super();
+        this.doors = new ArrayList<Door>();
+    }
 }
