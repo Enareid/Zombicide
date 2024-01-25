4ea055dbc6020ce66be2f1b5ad9acdc2a0460db1
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-01-25 13:54:45 +0100

    rename DrugStore.java in DrugStoreCell.java and constructor DrugStoreCell() in class DrugStoreCell.java

--- src/game/Celles/BuildingCells/DrugStore.java
+++ src/game/Celles/BuildingCells/DrugStore.java
@@ -3,5 +3,11 @@ package src.game.Celles.BuildingCells;
 import src.game.Celles.BuildingCell;
 
 public class DrugStore extends BuildingCell{
-    
+
+    /**
+     * Builds a new drug store cell. 
+     */
+    public DrugStore() {
+        super();
+    }
 }
