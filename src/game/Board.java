79225f82796f87a7c2e2075b8d3388bfd1df63a9
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-01-25 14:12:49 +0100

    method getCell(int x, int y) in class Board.java

--- src/game/Board.java
+++ src/game/Board.java
@@ -9,4 +9,14 @@ public class Board {
     public Board(int width, int height) {
         this.cells = new Cell[width][height];
     }
+
+    /**
+     * Returns the cell at the given coordinates. 
+     * @param x The x coordinate of the cell.
+     * @param y The y coordinate of the cell.
+     * @return The cell at the given coordinates.
+     */
+    public Cell getCell(int x, int y) {
+        return this.cells[x][y];
+    }
 }
