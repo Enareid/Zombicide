aef562363d764634f8acfc737b553f945a607787
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-01-26 22:24:49 +0100

    test

--- src/game/Boards/TrainingBoard.java
+++ src/game/Boards/TrainingBoard.java
@@ -1,6 +1,7 @@
 package src.game.Boards;
 
 import src.game.Board;
+import src.game.Celles.*;
 
 public class TrainingBoard extends Board{
 
@@ -10,4 +11,43 @@ public class TrainingBoard extends Board{
     public TrainingBoard() {
         super(5, 5);
     }
+
+    /**
+     * Initialize the board with cells
+     */
+    public void initBoard(){
+        for(int i = 0; i < this.cells.length; i++){
+            for(int j = 0; j < this.cells[i].length; j++){
+                if(i == 3 || j == 3) {
+                    this.cells[i][j] = new StreetCell();
+                }
+                else {
+                    this.cells[i][j] = new BuildingCell();
+                }
+            }
+        }
+    }
+
+    /**
+     * Board to string
+     */
+    public String toString(){
+        String str = "";
+        for(int i = 0; i < this.cells.length; i++){
+            for(int j = 0; j < this.cells[i].length; j++){
+                if (this.cells[i][j] instanceof BuildingCell) {
+                    str += "B";
+                }
+                else if (this.cells[i][j] instanceof StreetCell) {
+                    str += "S";
+                }
+                else {
+                    str += " ";
+                }
+            }
+            str += "\n";
+        }
+        return str;
+    }
+
 }
