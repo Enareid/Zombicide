af952a879f120da08bd5767ccac1514e0cc9f4b4
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-01-26 22:09:54 +0100

    Constructor TrainingBoard() in class TrainingBoard.java

--- src/game/Boards/TrainingBoard.java
+++ src/game/Boards/TrainingBoard.java
@@ -3,5 +3,11 @@ package src.game.Boards;
 import src.game.Board;
 
 public class TrainingBoard extends Board{
-    
+
+    /**
+     * Build an empty board 5x5
+     */
+    public TrainingBoard() {
+        super(5, 5);
+    }
 }
