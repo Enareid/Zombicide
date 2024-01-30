7dd0aa388c65150ca08ff8cb51804cc431ed41fb
Mazen Kalakech <Mazen Kalakech>
2024-01-30 08:30:38 +0100

    changed Board

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,13 +1,49 @@
 package src.game;
 
+import src.game.Celles.StreetCell;
+
 public class Board {
-    protected Cell[][] cells;
+
+    protected Cell[][] Board;
+    private int width;
+    private int height;
+    private static final char street='|';
+    private static final char building='B';
 
     /**
      * Build an empty board
      */
     public Board(int width, int height) {
-        this.cells = new Cell[width][height];
+        this.width = width;
+        this.height = height;
+        this.Board = new Cell[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                this.Board[x][y] = new Cell();
+            }
+        }
+    }
+
+    public Cell[][] decoup(Cell[][] board,int x1, int x2,int y1, int y2){
+        int length=x2-x1;
+        int width=y2-y1;
+        if (width<5 || length<5){
+            
+            return board;
+        }
+        else{
+            int random1 = (int)(Math.random()*length);
+            int random2 = (int)(Math.random()*width);
+            for(int j=y1;j<width;j++){
+                board[random1][j]=new StreetCell();
+            }
+            for(int i=x1;i<length;i++){
+                this.Board[i][random2]=new StreetCell();
+            }
+            decoup(board, x1, x2, y1, y2);
+            
+        }
+        return board;
     }
 
     /**
@@ -17,20 +53,15 @@ public class Board {
      * @return The cell at the given coordinates.
      */
     public Cell getCell(int x, int y) {
-        return this.cells[x][y];
+        return this.Board[x][y];
     }
 
     /**
      * Sets the cell at the given coordinates.
      */
     public void setCell(int x, int y, Cell cell) {
-        this.cells[x][y] = cell;
+        this.Board[x][y] = cell;
     }
 
-    /**
-     * Initialize the board with some rules and random cells.
-     */
-    public void initBoard(){
-        // TODO
-    }
 }
+
