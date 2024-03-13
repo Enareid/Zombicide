b7bbff9e5c5be18c3052033d67c1a053291834de
ALi <ali.kerbous.etu>
2024-03-13 20:59:53 +0100

    ajout Zombie

--- src/game/Entities/Zombie.java
+++ src/game/Entities/Zombie.java
@@ -5,11 +5,17 @@ import game.Entity;
 
 public abstract class Zombie extends Entity {
 
+    protected int Damage;
+    protected int actionPoint;
+
     /**
      * Builds a new zombie.
      */
     public Zombie(int Lifepoints, Cell cell){
         super(Lifepoints,cell);
     }
+    public int getDamage() {
+		return this.Damage;
+	}
     
 }
