51479b3884fc5b30158c206469356fbc144ccfb0
Mazen Kalakech <Mazen Kalakech>
2024-03-19 09:24:00 +0100

    Equipement test

--- src/game/Equipements/Items/HealingVial.java
+++ src/game/Equipements/Items/HealingVial.java
@@ -3,4 +3,11 @@ import game.Equipement;
 
 public class HealingVial extends Equipement{
     
+    protected int HealAmount;
+
+    public HealingVial(int HealAmount){
+        super();
+        this.HealAmount=HealAmount;
+    }
+    
 }
--- test/game/EquipementTest.java
+++ test/game/EquipementTest.java
@@ -1,17 +1,22 @@
+package game;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
+
+import game.Equipement;
+import game.Equipements.Items.MasterKey;
 
 public class EquipementTest {
 
     @Test
     public void testEquipementInitialization() {
-        Equipement equipement = new Equipement();
+        Equipement equipement = new MasterKey();
         assertFalse(equipement.isUsed());
     }
 
     @Test
     public void testEquipement() {
-        Equipement equipement = new Equipement();
+        Equipement equipement = new MasterKey();
         assertFalse(equipement.isUsed());
         
         equipement.use();
