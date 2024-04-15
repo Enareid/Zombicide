3e215dddd9ef88ce5050a126d12c272f502e6530
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:58:36 +0200

    doc HealingVial.java + ajout du constructeur

--- src/game/Equipements/Items/HealingVial.java
+++ src/game/Equipements/Items/HealingVial.java
@@ -4,12 +4,27 @@ import game.Entities.Player;
 
 public class HealingVial extends Equipement{
     
+    /**
+     * Builds a new HealingVial.
+     */
+    public HealingVial(){
+        super();
+    }
 
+    /**
+     * Uses the HealingVial.
+     * 
+     * @param player the player using the HealingVial
+     */
     public void use(Player player){
         player.setLifepoints(player.getLifepoints()+1);
         
     }
 
+    /**
+     * Returns the string representation of the HealingVial.
+     * @return the string representation of the HealingVial.
+     */
     public String toString() {
         return "HealingVial";
     }
