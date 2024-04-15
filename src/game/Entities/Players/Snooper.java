07a83ba75850310aeb9de334bc0d2db635f9793b
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:50:37 +0200

    doc Snooper.java

--- src/game/Entities/Players/Snooper.java
+++ src/game/Entities/Players/Snooper.java
@@ -6,10 +6,21 @@ import game.Entities.Player;
 
 public class Snooper extends Player{
 
+    /**
+     * Builds a new Snooper.
+     * 
+     * @param Lifepoints the lifepoints of the Snooper
+     * @param cell the cell where the Snooper will spawn
+     * @param board the board where the Snooper will spawn
+     */
     public Snooper(int Lifepoints, Cell cell,Board board){
         super(Lifepoints,cell, board);
     }
     
+    /**
+     * Returns the string representation of the Snooper.
+     * @return the string representation of the Snooper.
+     */
     public String toString() {
         return "Snooper";
     }
