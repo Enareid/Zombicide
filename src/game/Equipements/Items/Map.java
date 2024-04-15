b716e122af61e71afe00600552229adbed58df17
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:56:24 +0200

    doc Map.java + ajout du constructeur

--- src/game/Equipements/Items/Map.java
+++ src/game/Equipements/Items/Map.java
@@ -5,10 +5,26 @@ import game.Entities.Player;
 
  public class Map extends Equipement{
 
+    /**
+     * Builds a new Map.
+     */
+    public Map(){
+        super();
+    }
+
+    /**
+     * Uses the Map.
+     * 
+     * @param player the player using the Map
+     */
     public void use(Player player){
         player.getBoard().toString();
     }
 
+    /**
+     * Returns the string representation of the Map.
+     * @return the string representation of the Map.
+     */
     public String toString() {
         return "Map";
     }
