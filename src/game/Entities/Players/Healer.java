05fc7782ca6879fe2a5b5015c5543da2e1287beb
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:49:30 +0200

    doc Healer.java

--- src/game/Entities/Players/Healer.java
+++ src/game/Entities/Players/Healer.java
@@ -6,10 +6,21 @@ import game.Entities.Player;
 
 public class Healer extends Player{
 
+    /**
+     * Builds a new Healer.
+     * 
+     * @param Lifepoints the lifepoints of the Healer
+     * @param cell the cell where the Healer will spawn
+     * @param board the board where the Healer will spawn
+     */
     public Healer(int Lifepoints, Cell cell,Board board){
         super(Lifepoints,cell, board);
     }
     
+    /**
+     * Returns the string representation of the Healer.
+     * @return the string representation of the Healer.
+     */
     public String toString() {
         return "Healer";
     }
