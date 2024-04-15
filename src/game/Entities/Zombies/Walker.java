2c689ff680eb5120f5adf7a16b6ab71d1f518a58
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:48:20 +0200

    doc Walker.java

--- src/game/Entities/Zombies/Walker.java
+++ src/game/Entities/Zombies/Walker.java
@@ -5,10 +5,20 @@ import game.Board;
 
 public class Walker extends Zombie{
 
+    /**
+     * Builds a new Walker.
+     * 
+     * @param cell the cell where the Walker will spawn
+     * @param board the board where the Walker will spawn
+     */
     public Walker(Cell cell, Board board){
         super(1, 1, board, 1, false, cell,0);
     }
 
+    /**
+     * Returns the string representation of the Walker.
+     * @return the string representation of the Walker.
+     */
     public String toString(){
         return "Walker has "+this.getLifepoints()+" Lifepoints";
     }
