aabb1e370bcc6a72a84439af2a902ad8eee55281
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:59:22 +0200

    doc FirstAidKit.java + ajout du constructeur

--- src/game/Equipements/Items/FirstAidKit.java
+++ src/game/Equipements/Items/FirstAidKit.java
@@ -5,15 +5,27 @@ import game.Entities.Player;
 
 public class FirstAidKit extends Equipement {
 
+    /**
+     * Builds a new FirstAidKit.
+     */
     public FirstAidKit(){
         super();
     }
 
+    /**
+     * Uses the FirstAidKit.
+     * 
+     * @param receiver the player using the FirstAidKit
+     */
     public void use(Player receiver){
         receiver.setLifepoints(receiver.getLifepoints()+1);
         
     }
     
+    /**
+     * Returns the string representation of the FirstAidKit.
+     * @return the string representation of the FirstAidKit.
+     */
     public String toString() {
         return "FirstAidKit";
     }
