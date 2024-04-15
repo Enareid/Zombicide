6bf7238bf0007539cea242642c604cd9472c49c6
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:47:50 +0200

    doc Runner.java

--- src/game/Entities/Zombies/Runner.java
+++ src/game/Entities/Zombies/Runner.java
@@ -7,10 +7,20 @@ import game.Board;
 
 public class Runner extends Zombie{
 
+    /**
+     * Builds a new Runner.
+     * 
+     * @param cell the cell where the Runner will spawn
+     * @param board the board where the Runner will spawn
+     */
     public Runner(Cell cell, Board board){
         super(2, 1, board, 2, false, cell ,0);
     }
     
+    /**
+     * Returns the string representation of the Runner.
+     * @return the string representation of the Runner.
+     */
     public String toString(){
         return "Runner has "+this.getLifepoints()+" Lifepoints";
     }
