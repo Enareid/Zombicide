83a6fbefad29ccc2e91ec5a99661648fcc093afe
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:47:19 +0200

    doc Broom.java

--- src/game/Entities/Zombies/Broom.java
+++ src/game/Entities/Zombies/Broom.java
@@ -5,10 +5,20 @@ import game.Entities.Zombie;
 
 public class Broom extends Zombie{
     
+    /**
+     * Builds a new Broom.
+     * 
+     * @param cell the cell where the Broom will spawn
+     * @param board the board where the Broom will spawn
+     */
     public Broom(Cell cell, Board board){
         super(4, 2, board, 1, true, cell,1);
     }
 
+    /**
+     * Returns the string representation of the Broom.
+     * @return the string representation of the Broom.
+     */
     public String toString(){
         return "Broom has "+this.getLifepoints()+" Lifepoints";
     }
