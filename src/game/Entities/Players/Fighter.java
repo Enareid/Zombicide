799458fbf95935b2d913f041259067824218ef51
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-15 22:48:57 +0200

    doc Fighter.java

--- src/game/Entities/Players/Fighter.java
+++ src/game/Entities/Players/Fighter.java
@@ -6,10 +6,21 @@ import game.Entities.Player;
 
 public class Fighter extends Player{
     
+    /**
+     * Builds a new Fighter.
+     * 
+     * @param Lifepoints the lifepoints of the Fighter
+     * @param cell the cell where the Fighter will spawn
+     * @param board the board where the Fighter will spawn
+     */
     public Fighter(int Lifepoints, Cell cell,Board board){
         super(Lifepoints,cell,board);
     }
 
+    /**
+     * Returns the string representation of the Fighter.
+     * @return the string representation of the Fighter.
+     */
     public String toString() {
         return "Fighter";
     }
