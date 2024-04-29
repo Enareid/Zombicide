6feea88239238cf8899a0a7251f51d605b271629
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-04-29 17:33:31 +0200

    déplacement dans game

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,18 +1,41 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import game.Boards.ClassicalBoard;
 import game.Entities.Player;
 import game.Entities.Zombie;
+import game.Entities.Players.Fighter;
 
 public class Game {
     
     private Board board;
     private boolean isOver = false;
 
-    public Game(Board board) {
-        this.board = board;
+    public Game() {
+    }
+
+    public void init(){
+        List<Player> players = new ArrayList<Player>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez la taille du plateau:");
+        int size = scanner.nextInt();
+        Player p1 = new Fighter(null, null);
+        players.add(p1);
+        try{
+            Board board = new ClassicalBoard(size, players);
+            this.board = board;
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public void play(){
+        this.init();
+        board.spawnZombie();
         while(!isOver){
             for (Zombie zombie : board.getZombies()) {
                 zombie.action();
--- src/game/MainL1.java
+++ src/game/MainL1.java
@@ -1,52 +1,13 @@
 package game;
 
-import game.Boards.ClassicalBoard;
-import game.Entities.*;
-import game.Entities.Players.*;
-
-import game.Equipements.Items.InfraredGlasses;
-import game.Equipements.Weapons.*;
-import game.Equipements.Items.MasterKey;
-
-import java.util.*;
-
 public class MainL1 {
 
     public static void main(String[] args){
         int nbEssais = 0;
         while(nbEssais < 4){
             try {
-                List<Player> players = new ArrayList<Player>();
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Entrez la taille du plateau:");
-                int size = scanner.nextInt();
-                nbEssais++;
-                Player p1 = new Fighter(null, null);
-                players.add(p1);
-                Board board = new ClassicalBoard(size, players);
-                board.spawnZombie();
-                Equipement IG = new InfraredGlasses();
-                Equipement axe = new Axe();
-                Equipement gun = new Gun();
-                Equipement rifle = new Rifle();
-                Equipement Chainsaw = new Chainsaw();
-                Equipement Crowbar = new Crowbar();
-                Equipement Fiole = new InfraredGlasses();
-                MasterKey mk = new MasterKey();
-                p1.setEquipment(gun);
-                p1.setInHand(gun);
-                
-                System.out.println(board.toString());
-
-                for (int i = 0; i < 10; i++) {
-                    for (Zombie zombie : board.getZombies()) {
-                        zombie.action();
-                        System.out.println(p1.getLifepoints());
-                    }
-                    p1.action();            
-                }
-                scanner.close();
-                break;
+                Game game = new Game();
+                game.play();
             }
             catch(Exception e) {
                 System.out.println(e);
