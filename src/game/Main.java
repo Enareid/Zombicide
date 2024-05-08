096cc627bbd1e3e904ec963a3656702af3b6bc75
Stephane <stephane.boudier.etu@univ-lille.fr>
2024-05-08 11:00:59 +0200

    modification du main

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,6 +3,18 @@ package game;
 public class Main {
 
     public static void main(String[] args){
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar game.jar <size> <nbPlayers>");
+            System.exit(1);
+        }
+        if (Integer.parseInt(args[0]) < 5) {
+            System.out.println("Size must be at least 5");
+            System.exit(1);
+        }
+        if (Integer.parseInt(args[1]) < 1) {
+            System.out.println("Number of players must be at least 1");
+            System.exit(1);
+        }
         try {
             int size = Integer.parseInt(args[0]);
             int nbPlayers = Integer.parseInt(args[1]);
